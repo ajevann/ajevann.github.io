4a016a8198387e989a0718e5c7535f97a3d6623d
Alex Vann <ajevann@gmail.com>
2019-01-03 14:40:44 +0100

    reorganization of files, minor updates

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// Port
+var port = 8080;
+
+// Running locally, remove the base html tag that would mess up the loading of static files. gh-pages needs it though
+var baseRegex = /\<base href=\"\/rfp\/public\/">/g;
+var fs = require('fs');
+fs.readFile(path.resolve(__dirname + '/index.html'), 'utf8', function (err, data) {
+  fs.writeFile(path.resolve(__dirname + '/indexLocalHost.html'), data.replace(baseRegex, ''), 'utf8', function (err) {});
+});
+
+// Serve static files
+app.use(express.static(__dirname + '/public'));
+
+// Provide entry point
+app.get('/', function(req, res) {
+    res.sendFile(path.resolve(__dirname + '/indexLocalHost.html'));
+});
+
+app.listen(port);
+
+console.log('App running on port', port);
--- public/app/controllers/controllers.js
+++ public/app/controllers/controllers.js
@@ -3,6 +3,14 @@ var ajevannControllers = angular.module('ajevannControllers', []);
 ajevannControllers.controller('ProjectListCtrl', ['$scope', '$http',
   function ($scope, $http) {
     $http.get('assets/js/projects.json').success(function(data) {
+      
       $scope.projects = data;
+
+      var i = 1;
+      $scope.projects.forEach(project => {
+        project.order = i++;
+      });
+
     });
-  }]);
+  }
+]);
